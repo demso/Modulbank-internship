a71f6e51ba04400371756a2b5ec7cce9f862c2fd
dem <dmitriy313.k@gmail.com>
2025-07-31 12:04:22 +0300

    Fix JsonEnumSerialization not working

--- BankAccounts.Api/Program.cs
+++ BankAccounts.Api/Program.cs
@@ -1,27 +1,18 @@
 using System.Reflection;
 using System.Text.Json.Serialization;
-using AutoMapper;
-using BankAccounts.Api.Features.Accounts;
+using BankAccounts.Api;
 using BankAccounts.Api.Infrastructure;
-using BankAccounts.Api.Mapping;
-using Microsoft.AspNetCore.Http.Json;
-using Microsoft.EntityFrameworkCore.Storage.Json;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 try
 {
     builder.Services
-        .Configure<JsonOptions>(options => options
-            .SerializerOptions.Converters.Add(new JsonStringEnumConverter()))
         .AddDbContext<BankAccountsContext>()
-        .AddScoped<IBankAccountsContext>(provider =>
-            provider.GetRequiredService<BankAccountsContext>()!)
-        .AddAutoMapper(options => options
-            .AddProfile(new AccountMappingProfile()))
-        .AddMediatR(options => options
-            .RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
-        .AddControllers();
+        .AddScoped<IBankAccountsContext>(provider => provider.GetRequiredService<BankAccountsContext>()!)
+        .AddAutoMapper(options => options.AddProfile(new MappingProfile()))
+        .AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+        .AddControllers()
+        .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 }
 catch (Exception exception)
 {
@@ -38,13 +29,8 @@ builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
 var app = builder.Build();
 
 app.UseRouting();
-//app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 
 app.MapControllers();
-
-//var scope = app.Services.CreateScope();
-//var dbContext = scope.ServiceProvider.GetRequiredService<BankAccountsContext>();
-//dbContext.Database.EnsureCreated();
 
 app.Run();
