67d796e39388660e912cb1f543fe27b176a889f8
dem <dmitriy313.k@gmail.com>
2025-07-31 12:01:52 +0300

    Optimize mapping

---
+++ BankAccounts.Api/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BankAccounts.Api.Features.Accounts;
+using BankAccounts.Api.Features.Accounts.Commands;
+using BankAccounts.Api.Features.Accounts.Dtos;
+using BankAccounts.Api.Features.Accounts.Queries;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BankAccounts.Api;
+
+public class MappingProfile : Profile
+{
+    /// <summary>
+    /// Инициализирует новый экземпляр <see cref="MappingProfile"/>,
+    /// настраивая правила преобразования между <see cref="AccountCreateDto"/> и <see cref="Account"/>,
+    /// а также между <see cref="Account"/> и <see cref="AccountDto"/>.
+    /// </summary>
+    public MappingProfile()
+    {
+        CreateMap<Account, AccountDto>();
+
+        CreateMap<CreateAccountDto, CreateAccount.Command>();
+    }
+}
