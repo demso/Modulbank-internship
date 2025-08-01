62da5791ec7a305c55545141355dd7fe47a269f7
dem <dmitriy313.k@gmail.com>
2025-08-01 13:04:08 +0300

    Configure authentication services

---
+++ BankAccounts.Api/Identity/Configuration.cs
@@ -0,0 +1,63 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace BankAccounts.Api.Identity;
+
+public class Configuration
+{
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("BankAccountsWebAPI", "Web API")
+        };
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new List<ApiResource>
+        {
+            new ApiResource("BankAccountsWebAPI", "Web API", new []
+                { JwtClaimTypes.Name})
+            {
+                Scopes = { "BankAccountsWebAPI" }
+            }
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "bank-accounts-web-app",
+                ClientName = "Bank Accounts Web",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequireClientSecret = false,
+                RequirePkce = true,
+                RedirectUris =
+                {
+                    "http://localhost:80/signin-oidc"
+                },
+                AllowedCorsOrigins =
+                {
+                    "http://localhost:80"
+                },
+                PostLogoutRedirectUris =
+                {
+                    "http://localhost:80/signout-oidc"
+                },
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "BankAccountsWebAPI"
+                },
+                AllowAccessTokensViaBrowser = true
+            }
+        };
+}
--- BankAccounts.Api/Program.cs
+++ BankAccounts.Api/Program.cs
@@ -2,29 +2,45 @@ using System.Reflection;
 using System.Text.Json.Serialization;
 using BankAccounts.Api;
 using BankAccounts.Api.Features;
+using BankAccounts.Api.Identity;
 using BankAccounts.Api.Infrastructure;
 using FluentValidation;
 using IdentityServer4.Models;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddDbContext<BankAccountsContext>()
+    .AddDbContext<BankAccountsDbContext>()
+    .AddDbContext<AuthDbContext>()
     .AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()])
     .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
-    .AddScoped<IBankAccountsContext>(provider => provider.GetRequiredService<BankAccountsContext>()!)
+    .AddScoped<IBankAccountsDbContext>(provider => provider.GetRequiredService<BankAccountsDbContext>()!)
     .AddAutoMapper(options => options.AddProfile(new MappingProfile()))
     .AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
     .AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
+builder.Services.AddIdentity<BankUser, IdentityUser>(config =>
+    {
+        config.Password.RequiredLength = 4;
+        config.Password.RequireDigit = false;
+        config.Password.RequireNonAlphanumeric = false;
+        config.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddIdentityServer()
-    .AddInMemoryApiResources(new List<ApiResource>())
-    .AddInMemoryIdentityResources(new List<IdentityResource>())
-    .AddInMemoryApiScopes(new List<ApiScope>())
-    .AddInMemoryClients(new List<Client>())
+    .AddAspNetIdentity<BankUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
 {
@@ -34,7 +50,6 @@ builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
 }));
 
 var app = builder.Build();
-
 
 app.UseRouting();
 app.UseIdentityServer();
