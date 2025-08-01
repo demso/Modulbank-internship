2955a766c592af0f9ba11e738f4b965e44798509
dem <dmitriy313.k@gmail.com>
2025-08-02 00:00:38 +0300

    Remove OwnerId from CreateAccountDto

--- BankAccounts.Api/Features/Accounts/Dtos/CreateAccountDto.cs
+++ BankAccounts.Api/Features/Accounts/Dtos/CreateAccountDto.cs
@@ -3,7 +3,6 @@
 namespace BankAccounts.Api.Features.Accounts.Dtos;
 
 public record CreateAccountDto(
-    [Required] Guid? OwnerId,
     [Required] AccountType? AccountType,
     [Required] CurrencyService.Currencies? Currency,
      decimal? InterestRate
