a3d20cb0f311cafd9373196a8be3514a279795be
dem <dmitriy313.k@gmail.com>
2025-08-01 13:00:54 +0300

    Add AuthDbContext

---
+++ BankAccounts.Api/Identity/AuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Authorization.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAccounts.Api.Identity;
+
+public class AuthDbContext : IdentityDbContext<BankUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<BankUser>(entity => entity.ToTable(name: "Users"));
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+            entity.ToTable(name: "UserClaim"));
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+            entity.ToTable("UserLogins"));
+        builder.Entity<IdentityUserToken<string>>(entity =>
+            entity.ToTable("UserTokens"));
+
+        builder.ApplyConfiguration(new BankUserConfiguration());
+    }
+}
