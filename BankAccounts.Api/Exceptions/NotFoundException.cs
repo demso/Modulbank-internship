30a7e2387f027a0862e1aa5334da87dd6052415e
dem <dmitriy313.k@gmail.com>
2025-08-01 23:54:19 +0300

    Update NotFoundException.cs to have details field

--- BankAccounts.Api/Exceptions/NotFoundException.cs
+++ BankAccounts.Api/Exceptions/NotFoundException.cs
@@ -1,3 +1,3 @@
 ﻿namespace BankAccounts.Api.Exceptions;
 
-public class NotFoundException(string name, object key) : Exception($"Exception \"{name}\" ({key}) not found.");
+public class NotFoundException(string name, object key, string details = "") : Exception($"Exception \"{name}\" ({key}) not found. " + details);
