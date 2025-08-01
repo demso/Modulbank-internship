8759ff06b48cf1a5388aef035da1a8e253c76a40
dem <dmitriy313.k@gmail.com>
2025-08-01 18:38:36 +0300

    Cleanup AuthDbContext

--- BankAccounts.Api/Identity/AuthDbContext.cs
+++ BankAccounts.Api/Identity/AuthDbContext.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using BankAccounts.Api.Features.Accounts;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BankAccounts.Api.Identity;
 
-public class AuthDbContext : IdentityDbContext<BankUser>
+public class AuthDbContext(DbContextOptions<AuthDbContext> options) : IdentityDbContext<BankUser>(options)
 {
-    public AuthDbContext(DbContextOptions<AuthDbContext> options)
-        : base(options) { }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase("AuthenticationDatabase");
@@ -17,15 +15,6 @@ public class AuthDbContext : IdentityDbContext<BankUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<BankUser>(entity => entity.ToTable(name: "Users"));
-        builder.Entity<IdentityUserClaim<string>>(entity =>
-            entity.ToTable(name: "UserClaim"));
-        builder.Entity<IdentityUserLogin<string>>(entity =>
-            entity.ToTable("UserLogins"));
-        builder.Entity<IdentityUserToken<string>>(entity =>
-            entity.ToTable("UserTokens"));
-
         builder.ApplyConfiguration(new BankUserConfiguration());
     }
 }
