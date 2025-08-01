1db28dbf946fcf47a3af33cb6ce751b8924e40b3
dem <dmitriy313.k@gmail.com>
2025-08-01 23:58:01 +0300

    Update DeleteAccount.cs Handler to have Unit return value

--- BankAccounts.Api/Features/Accounts/Commands/DeleteAccount.cs
+++ BankAccounts.Api/Features/Accounts/Commands/DeleteAccount.cs
@@ -11,11 +11,11 @@ public static class DeleteAccount
     public record Command(
         Guid OwnerId,
         int AccountId
-    ) : IRequest;
+    ) : IRequest<Unit>;
 
-    public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : IRequestHandler<Command>
+    public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : IRequestHandler<Command, Unit>
     {
-        public async Task Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
             var account = await dbDbContext.Accounts.FindAsync(request.AccountId, cancellationToken);
             if (account == null || account.OwnerId != request.OwnerId)
@@ -24,12 +24,13 @@ public static class DeleteAccount
                 throw new Exception("Невозможно удалить счет пока баланс больше 0.");
             dbDbContext.Accounts.Remove(account);
             await dbDbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
         }
     }
 
     public class CommandValidator : AbstractValidator<Command>
     {
-        public CommandValidator(IBankAccountsDbContext dbDbContext)
+        public CommandValidator()
         {
             RuleFor(command => command.OwnerId).NotEqual(Guid.Empty);
             RuleFor(command => command.AccountId).GreaterThan(0);
