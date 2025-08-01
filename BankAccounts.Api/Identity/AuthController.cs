00e55db78d23e623f9fe82ed5da13c3eb8269c3e
dem <dmitriy313.k@gmail.com>
2025-08-01 18:38:16 +0300

    Add generation and return authetication token to AuthController

--- BankAccounts.Api/Identity/AuthController.cs
+++ BankAccounts.Api/Identity/AuthController.cs
@@ -1,7 +1,12 @@
 ﻿using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Notes.Identity.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BankAccounts.Api.Identity;
 
@@ -10,7 +15,7 @@ namespace BankAccounts.Api.Identity;
 public class AuthController(
     SignInManager<BankUser> signInManager,
     UserManager<BankUser> userManager,
-    IIdentityServerInteractionService interactionService)
+    IIdentityServerInteractionService interactionService, IConfiguration configuration, IdentityDbContext<BankUser> iDbContext)
     : Controller
 {
     [HttpPost]
@@ -24,18 +29,46 @@ public class AuthController(
         if (!result.Succeeded)
             return Problem("Login failed.");
 
-        return Ok("Login succeed.");
+        var token = GenerateJwtToken(data.Username);
+
+        return Ok(token);
+    }
+
+    private string GenerateJwtToken(string username)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, username)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["Jwt:Issuer"],
+            audience: configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(60),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
     [HttpPost]
-    public async Task<ActionResult> Register(RegisterData data) {
+    public async Task<ActionResult> Register(RegisterData data)
+    {
         var user = new BankUser {
             UserName = data.Username
         };
 
         var result = await userManager.CreateAsync(user, data.Password);
         if (!result.Succeeded)
-            return Problem("Register error.");
+        {
+            var errorMessage = string.Join("\n | ", result.Errors.Select(error => error.Description));
+            return Problem(errorMessage);
+        }
         
         await signInManager.SignInAsync(user, false);
 
