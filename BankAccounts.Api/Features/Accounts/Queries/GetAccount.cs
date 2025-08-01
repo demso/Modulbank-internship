fe109f1d8b078102d6194ae1a0e19b20391f1a72
dem <dmitriy313.k@gmail.com>
2025-08-02 00:03:44 +0300

    Refactor and clean uo GetAccount.cs

--- BankAccounts.Api/Features/Accounts/Queries/GetAccount.cs
+++ BankAccounts.Api/Features/Accounts/Queries/GetAccount.cs
@@ -6,37 +6,37 @@ using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace BankAccounts.Api.Features.Accounts.Queries
+namespace BankAccounts.Api.Features.Accounts.Queries;
+
+public static class GetAccount
 {
-    public static class GetAccount
+    public record Query(
+        Guid OwnerId,
+        int AccountId
+    ) : IRequest<AccountDto>;
+    
+    public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : IRequestHandler<Query, AccountDto>
     {
-        public record Query(
-            int AccountId,
-            Guid OwnerId
-        ) : IRequest<AccountDto>;
-        
-        public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : IRequestHandler<Query, AccountDto>
+        public async Task<AccountDto> Handle(Query request, CancellationToken cancellationToken)
         {
-            public async Task<AccountDto> Handle(Query request, CancellationToken cancellationToken)
+            var entity = await dbDbContext.Accounts.FirstOrDefaultAsync(account => 
+                account.AccountId == request.AccountId && account.OwnerId == request.OwnerId, cancellationToken);
+            if (entity == null || !request.OwnerId.Equals(entity.OwnerId))
             {
-                var entity = await dbDbContext.Accounts.FirstOrDefaultAsync(account => 
-                    account.AccountId == request.AccountId && account.OwnerId == request.OwnerId, cancellationToken);
-                if (entity == null || !request.OwnerId.Equals(entity.OwnerId))
-                {
-                    throw new NotFoundException(nameof(Account), request.AccountId);
-                }
-
-                return mapper.Map<AccountDto>(entity);
+                throw new NotFoundException(nameof(Account), request.AccountId);
             }
+
+            return mapper.Map<AccountDto>(entity);
         }
+    }
 
-        public class QueryValidator : AbstractValidator<Query>
+    public class QueryValidator : AbstractValidator<Query>
+    {
+        public QueryValidator(IBankAccountsDbContext dbDbContext)
         {
-            public QueryValidator(IBankAccountsDbContext dbDbContext)
-            {
-                RuleFor(command => command.OwnerId).NotEqual(Guid.Empty);
-                RuleFor(command => command.AccountId).GreaterThan(0);
-            }
+            RuleFor(command => command.OwnerId).NotEqual(Guid.Empty);
+            RuleFor(command => command.AccountId).GreaterThan(0);
         }
     }
 }
+
