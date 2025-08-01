be225e85ad2c13aae4b974d949c145d691e9526d
dem <dmitriy313.k@gmail.com>
2025-08-01 13:03:50 +0300

    Add BankUser LiginData RegisterData

---
+++ BankAccounts.Api/Identity/BankUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BankAccounts.Api.Identity;
+
+public class BankUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
