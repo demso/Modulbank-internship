bd7b09fc3f1fe047affc7f7bd9ad85b624c7d45c
dem <dmitriy313.k@gmail.com>
2025-08-01 18:42:14 +0300

    Update project and https

--- BankAccounts.Api/Program.cs
+++ BankAccounts.Api/Program.cs
@@ -1,13 +1,16 @@
-using System.Reflection;
-using System.Text.Json.Serialization;
 using BankAccounts.Api;
 using BankAccounts.Api.Features;
 using BankAccounts.Api.Identity;
 using BankAccounts.Api.Infrastructure;
 using FluentValidation;
-using IdentityServer4.Models;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,17 +20,19 @@ builder.Services
     .AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()])
     .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
     .AddScoped<IBankAccountsDbContext>(provider => provider.GetRequiredService<BankAccountsDbContext>()!)
+    .AddScoped<IdentityDbContext<BankUser>>(provider => provider.GetRequiredService<AuthDbContext>()!)
     .AddAutoMapper(options => options.AddProfile(new MappingProfile()))
     .AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
     .AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-builder.Services.AddIdentity<BankUser, IdentityUser>(config =>
+builder.Services.AddIdentity<BankUser, IdentityRole>(config =>
     {
         config.Password.RequiredLength = 4;
         config.Password.RequireDigit = false;
         config.Password.RequireNonAlphanumeric = false;
         config.Password.RequireUppercase = false;
+        config.Password.RequireLowercase = false;
     })
     .AddEntityFrameworkStores<AuthDbContext>()
     .AddDefaultTokenProviders();
@@ -40,8 +45,6 @@ builder.Services.AddIdentityServer()
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
 
-builder.Services.AddControllersWithViews();
-
 builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
 {
     policy.AllowAnyHeader();
@@ -49,13 +52,40 @@ builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
     policy.AllowAnyOrigin();
 }));
 
+builder.Services.AddAuthentication(config =>
+    {
+        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+
 var app = builder.Build();
 
 app.UseRouting();
+app.UseHttpsRedirection();
 app.UseIdentityServer();
 app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseMiddleware<CustomExceptionHandlerMiddleware>();
 
 app.MapControllers();
 
 app.Run();
+
