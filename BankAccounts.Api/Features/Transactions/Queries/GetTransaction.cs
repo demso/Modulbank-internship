c55981a5f2085fc779e0c2b8f60fa1e9ecdc8c55
dem <dmitriy313.k@gmail.com>
2025-08-02 10:00:49 +0300

    Update GetTransaction Handler to have base class BaseRequestHandler

--- BankAccounts.Api/Features/Transactions/Queries/GetTransaction.cs
+++ BankAccounts.Api/Features/Transactions/Queries/GetTransaction.cs
@@ -2,6 +2,7 @@
 using BankAccounts.Api.Exceptions;
 using BankAccounts.Api.Features.Transactions.Dtos;
 using BankAccounts.Api.Infrastructure;
+using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,18 +10,26 @@ namespace BankAccounts.Api.Features.Transactions.Queries;
 
 public static class GetTransaction
 {
-    public record Query(Guid TransactionId) : IRequest<TransactionDto>;
+    public record Query(
+        Guid OwnerId,
+        Guid TransactionId
+    ) : IRequest<TransactionDto>;
 
-    public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : IRequestHandler<Query, TransactionDto>
+    public class Handler(IBankAccountsDbContext dbDbContext, IMapper mapper) : BaseRequestHandler<Query, TransactionDto>
     {
-        public async Task<TransactionDto> Handle(Query request, CancellationToken cancellationToken)
+        public override async Task<TransactionDto> Handle(Query request, CancellationToken cancellationToken)
         {
-            var entity = await dbDbContext.Transactions.FirstOrDefaultAsync(transaction =>
+            var transaction = await dbDbContext.Transactions.FirstOrDefaultAsync(transaction =>
                 transaction.TransactionId == request.TransactionId, cancellationToken);
-            if (entity == null)
-            {
+
+            if (transaction == null)
                 throw new NotFoundException(nameof(Transaction), request.TransactionId);
-            }
+
+            await GetValidAccount(dbDbContext, transaction.AccountId, request.OwnerId, cancellationToken);
+
+            return mapper.Map<TransactionDto>(transaction);
+        }
+    }
 
     public class QueryValidator : AbstractValidator<Query>
     {
@@ -30,5 +39,4 @@ public static class GetTransaction
             RuleFor(command => command.TransactionId).NotEmpty();
         }
     }
-    
 }
