e65ccd93ba4633cd0e30a58e677b1a8675a64ac4
dem <dmitriy313.k@gmail.com>
2025-08-02 15:41:11 +0300

    Update AccountsController to use MbResult

--- BankAccounts.Api/Features/Accounts/AccountsController.cs
+++ BankAccounts.Api/Features/Accounts/AccountsController.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Claims;
-using AutoMapper;
+﻿using AutoMapper;
 using BankAccounts.Api.Features.Accounts.Commands;
 using BankAccounts.Api.Features.Accounts.Dtos;
 using BankAccounts.Api.Features.Accounts.Queries;
@@ -14,96 +13,91 @@ namespace BankAccounts.Api.Features.Accounts;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AccountsController(IMapper mapper, IMediator mediator) : ControllerBase
+public class AccountsController(IMapper mapper, IMediator mediator) : CustomControllerBase
 {
     [HttpGet("all")]
     [Authorize]
-    public async Task<ActionResult<List<AccountDto>>> GetAllAccounts()
+    public async Task<MbResult<List<AccountDto>>> GetAllAccounts()
     {
         var query = new GetAllAccountsForUser.Query(GetUserGuid());
         var accountList = await mediator.Send(query);
-        return Ok(accountList);
+        return Success(StatusCodes.Status200OK, accountList);
     }
 
     [HttpGet("{accountId:int}")]
     [Authorize]
-    public async Task<ActionResult<AccountDto>> GetAccount(int accountId)
+    public async Task<MbResult<AccountDto>> GetAccount(int accountId)
     {
         var query = new GetAccount.Query(GetUserGuid(), accountId);
         var account = await mediator.Send(query);
-        return Ok(account);
+        return Success(StatusCodes.Status200OK, account);
     }
 
     [HttpDelete("{accountId:int}")]
     [Authorize]
-    public async Task<ActionResult> DeleteAccount(int accountId)
+    public async Task<MbResult> DeleteAccount(int accountId)
     {
         var command = new DeleteAccount.Command(GetUserGuid(), accountId);
         await mediator.Send(command);
-        return NoContent();
+        return Success(StatusCodes.Status204NoContent);
     }
 
     [HttpPatch("{accountId:int}")]
     [Authorize]
-    public async Task<ActionResult> UpdateAccount(int accountId, [FromQuery] decimal? interestRate, [FromQuery] bool close)
+    public async Task<MbResult> UpdateAccount(int accountId, [FromQuery] decimal? interestRate, [FromQuery] bool close)
     {
         var command = new UpdateAccount.Command(GetUserGuid(), accountId, interestRate, close);
         await mediator.Send(command);
-        return NoContent();
+        return Success(StatusCodes.Status204NoContent);
     }
 
     [HttpPost]
     [Authorize]
-    public async Task<ActionResult<AccountDto>> CreateAccount([FromBody] CreateAccountDto createAccountDto)
+    public async Task<MbResult<AccountDto>> CreateAccount([FromBody] CreateAccountDto createAccountDto)
     {
         var command = mapper.Map<CreateAccount.Command>(createAccountDto);
         command.OwnerId = GetUserGuid();
         var result = await mediator.Send(command);
-        return CreatedAtAction(nameof(GetAccount), new { accountId = result.AccountId}, result);
+        return Success(StatusCodes.Status201Created, result);
     }
 
     [HttpGet("{accountId}/transactions")]
     [Authorize]
-    public async Task<ActionResult<List<TransactionDto>>> GetTransactionsForAccount(int accountId,
+    public async Task<MbResult<List<TransactionDto>>> GetTransactionsForAccount(int accountId,
         [FromQuery] DateOnly? fromDate, DateOnly? toDate)
     {
         var query = new GetTransactionsForAccount.Query(GetUserGuid(), accountId, fromDate, toDate);
         var transactionList = await mediator.Send(query);
-        return Ok(transactionList);
+        return Success(StatusCodes.Status200OK, transactionList);
     }
 
     [HttpGet("transactions/{transactionId:guid}")]
     [Authorize]
-    public async Task<ActionResult<TransactionDto>> GetTransaction(Guid transactionId)
+    public async Task<MbResult<TransactionDto>> GetTransaction(Guid transactionId)
     {
         var query = new GetTransaction.Query(GetUserGuid(), transactionId);
         var transaction = await mediator.Send(query);
-        return Ok(transaction);
+        return Success(StatusCodes.Status200OK, transaction);
     }
 
     [HttpPost("transactions")]
     [Authorize]
-    public async Task<ActionResult<TransactionDto>> PerformTransaction([FromBody] PerformTransactionDto performTransactionDto)
+    public async Task<MbResult<TransactionDto>> PerformTransaction([FromBody] PerformTransactionDto performTransactionDto)
     {
         var command = mapper.Map<PerformTransaction.Command>(performTransactionDto);
         command.OwnerId = GetUserGuid();
         var result = await mediator.Send(command);
-        return CreatedAtAction(nameof(GetTransaction), new {transactionId = result.TransactionId}, result);
+        return Success(StatusCodes.Status201Created, result);
     }
 
     [HttpPost("transfer")]
     [Authorize]
-    public async Task<ActionResult<TransactionDto>> PerformTransfer([FromBody] PerformTransferDto performTransferDto)
+    public async Task<MbResult<TransactionDto>> PerformTransfer([FromBody] PerformTransferDto performTransferDto)
     {
         var command = mapper.Map<PerformTransfer.Command>(performTransferDto);
         command.OwnerId = GetUserGuid();
         var result = await mediator.Send(command);
-        return CreatedAtAction(nameof(GetAccount), new { accountId = result.AccountId}, result);
-    }
-
-    private Guid GetUserGuid()
-    {
-        return Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+        return Success(StatusCodes.Status201Created, result);
     }
 }
 
