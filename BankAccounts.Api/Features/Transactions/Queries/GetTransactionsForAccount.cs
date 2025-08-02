def0113ef468dad0ff741cfcfd0ad8d8279fa447
dem <dmitriy313.k@gmail.com>
2025-08-02 11:10:42 +0300

    Update GetTransacionsForAccount.cs validator to check dates

--- BankAccounts.Api/Features/Transactions/Queries/GetTransactionsForAccount.cs
+++ BankAccounts.Api/Features/Transactions/Queries/GetTransactionsForAccount.cs
@@ -39,14 +39,16 @@ public static class GetTransactionsForAccount
     {
         public QueryValidator()
         {
-            RuleFor(command => command.AccountId).GreaterThan(0);
-            RuleFor(command => command.FromDate)
+            RuleFor(query => query.AccountId).GreaterThan(0);
+            RuleFor(query => query.FromDate)
                 .GreaterThan(new DateOnly(1900, 1, 1))
+                .When(query => query.FromDate is not null)
                 .DependentRules(() =>
-                    RuleFor(command => command.ToDate)
-                        .GreaterThan(command => command.FromDate)
-                        .WithMessage("Конец периода должен быть позже начала периода.")
-                );
+                    RuleFor(query => query.ToDate)
+                        .GreaterThan(query => query.FromDate)
+                        .When(query => query.ToDate is not null)
+                        .WithMessage("Конец периода должен быть позже начала периода."))
+                .When(query => query.FromDate is not null);
            
         }
     }
