d38ad5f5e5abbb48df28c61878b66fd166e42220
dem <dmitriy313.k@gmail.com>
2025-08-02 09:03:36 +0300

    Add AccountNotFoundException

---
+++ BankAccounts.Api/Exceptions/AccountNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿using BankAccounts.Api.Features.Accounts;
+
+namespace BankAccounts.Api.Exceptions;
+
+public class AccountNotFoundException(int accountId) 
+    : NotFoundException(nameof(Account), accountId, "У вас нет такого счета.") { }
