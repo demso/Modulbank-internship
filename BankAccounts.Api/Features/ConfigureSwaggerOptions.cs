50b56307cf3827c9bca8db1574a98fbaa20ee513
dem <dmitriy313.k@gmail.com>
2025-08-02 21:13:00 +0300

    Create ConfigureSwaggerOptions.cs

---
+++ BankAccounts.Api/Features/ConfigureSwaggerOptions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace BankAccounts.Api.Features;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.AddSecurityDefinition($"AuthToken",
+            new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer",
+                Name = "Authorization",
+                Description = "Authorization token"
+            });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = $"AuthToken"
+                        }
+                    },
+                    []
+                }
+            });
+
+        options.CustomOperationIds(apiDescription =>
+            apiDescription.TryGetMethodInfo(out var methodInfo)
+                ? methodInfo.Name
+                : null);
+    }
+}
