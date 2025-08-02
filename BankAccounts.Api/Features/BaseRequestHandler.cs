f5a939ff670134d8ecfb1ff6fda3d1bf93b11219
dem <dmitriy313.k@gmail.com>
2025-08-02 09:54:46 +0300

    Add BaseRequestHandler

---
+++ BankAccounts.Api/Features/BaseRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using BankAccounts.Api.Exceptions;
+using BankAccounts.Api.Features.Accounts;
+using BankAccounts.Api.Infrastructure;
+using MediatR;
+
+namespace BankAccounts.Api.Features;
+
+public abstract class BaseRequestHandler<TRequest, TResponse>
+     : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+
+    public async Task<Account> GetValidAccount(IBankAccountsDbContext dbContext, int accountId, Guid ownerId, CancellationToken cancellationToken)
+    {
+        var account = await dbContext.Accounts.FindAsync(accountId, cancellationToken);
+
+        if (account == null || account.OwnerId != ownerId)
+            throw new AccountNotFoundException(accountId);
+
+        return account;
+    }
+}
