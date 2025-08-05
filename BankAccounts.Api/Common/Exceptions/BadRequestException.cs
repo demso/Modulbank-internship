5f15439c55e4ae8e0c0dcba761dd96160282e6c7
dem <dmitriy313.k@gmail.com>
2025-08-05 17:24:23 +0300

    feat: добавит новй тип исключения BadRequestException

---
+++ BankAccounts.Api/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,7 @@
+﻿namespace BankAccounts.Api.Common.Exceptions;
+
+/// <summary>
+/// Исключение вбрасываемое в случае неверных входных данных, но их нецелесообразно или невозсожно проверить в валидаторе
+/// </summary>
+/// <param name="details">Описание ошибки</param>
+public class BadRequestException(string details = "") : Exception("Неверный запрос. " + details);
