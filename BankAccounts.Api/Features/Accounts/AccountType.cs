cb55ee3bcf9d5253a2d2f950eb2bece5b1e58888
dem <dmitriy313.k@gmail.com>
2025-08-05 17:23:36 +0300

    docs: добавит документацию к перечислению AccountType

--- BankAccounts.Api/Features/Accounts/AccountType.cs
+++ BankAccounts.Api/Features/Accounts/AccountType.cs
@@ -1,9 +1,21 @@
-﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-namespace BankAccounts.Api.Features.Accounts;
+﻿namespace BankAccounts.Api.Features.Accounts;
 
+/// <summary>
+/// Тип аккаунта
+/// </summary>
 public enum AccountType
 {
+    /// <summary>
+    /// Текущий счет
+    /// </summary>
     Checking,
+    /// <summary>
+    /// Депозитный счет
+    /// </summary>
     Deposit,
+    /// <summary>
+    /// Крелитный счет
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global Тип используется
     Credit
 }
