9d7b64d9fc11166bbf87514c1380e76f49360266
dem <dmitriy313.k@gmail.com>
2025-08-08 10:13:02 +0300

    feat: добавит новый тип исключения ConcurrencyException

---
+++ BankAccounts.Api/Common/Exceptions/ConcurrencyException.cs
@@ -0,0 +1,5 @@
+﻿namespace BankAccounts.Api.Common.Exceptions;
+
+public class ConcurrencyException(string message, Exception innerException) 
+    : InvalidOperationException(message, innerException)
+{ }
