3ca3c0b412fc9954cf8ea260cf61b7bd681b6136
dem <dmitriy313.k@gmail.com>
2025-08-09 11:30:53 +0300

    feat: добавит сортировку счетов для удобного отображения в GetAllAccountsForUserHandler

--- BankAccounts.Api/Features/Accounts/Queries/GetAllAccountsForUser/GetAllAccountsForUserHandler.cs
+++ BankAccounts.Api/Features/Accounts/Queries/GetAllAccountsForUser/GetAllAccountsForUserHandler.cs
@@ -14,7 +14,10 @@ public class GetAllAccountsForUserHandler(IAccountsRepositoryAsync accountsRepos
     public override async Task<List<AccountDto>> Handle(GetAllCountsForUserQuery request, CancellationToken cancellationToken)
     {
         var entities = await accountsRepository.GetByFilterAsync(request.OwnerId, cancellationToken);
-        
+        // сортируем счета для удобного отображения
+        entities.Sort((a1, a2) =>
+            a1.AccountId > a2.AccountId ? 1 : a1.AccountId == a2.AccountId ? 0 : -1);
+
         var entitiesDto = mapper.Map<List<AccountDto>>(entities);
 
         return entitiesDto;
