64040808d6604d585a840305cbd0c188de06ffff
dem <dmitriy313.k@gmail.com>
2025-08-13 21:41:16 +0300

    task4: добавит новый тип исключения TransferException

---
+++ BankAccounts.Api/Common/Exceptions/TransferException.cs
@@ -0,0 +1,14 @@
+﻿using BankAccounts.Api.Features.Transactions.Commands.PerformTransfer;
+
+namespace BankAccounts.Api.Common.Exceptions
+{
+    /// <summary>
+    /// Исключение выбрасываемое в <see cref="PerformTransferHandler"/> при ошибке трансфера
+    /// </summary>
+    /// <param name="message">Сообщение</param>
+    /// <param name="ex">Исключение</param>
+    public class TransferException(string message, Exception ex) : Exception(message, ex)
+    {
+        
+    }
+}
