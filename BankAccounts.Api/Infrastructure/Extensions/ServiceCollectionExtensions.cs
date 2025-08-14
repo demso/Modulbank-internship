d8ca84dfd46a41d0e58b9e97bbf31c2242dd5df2
dem <dmitriy313.k@gmail.com>
2025-08-14 14:48:09 +0300

    task4: добавит ServiceCollectionExtensions.cs и перенесет добавление и конфигурацию сервисов в него, изменит хранилище для Hangfire с внутреннего на Postgres

---
+++ BankAccounts.Api/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,164 @@
+﻿using BankAccounts.Api.Common;
+using BankAccounts.Api.Features.Accounts;
+using BankAccounts.Api.Features.Transactions;
+using BankAccounts.Api.Infrastructure.CurrencyService;
+using BankAccounts.Api.Infrastructure.Database.Context;
+using BankAccounts.Api.Infrastructure.Hangfire.Registrator;
+using BankAccounts.Api.Infrastructure.Repository.Accounts;
+using BankAccounts.Api.Infrastructure.Repository.Transactions;
+using FluentValidation;
+using Hangfire;
+using Hangfire.PostgreSql;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BankAccounts.Api.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Набор методов расширения IServiceCollection для добавления и настройки необходимых компонентов для сервиса банковских счетов
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Добавит компоненты, не требующие сложной настройки
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCommonServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services
+                .AddDbContext<BankAccountsDbContext>(optionsBuilder => 
+                    optionsBuilder.UseNpgsql(configuration.GetConnectionString(nameof(BankAccountsDbContext)), options =>
+                    {
+                        options.MapEnum<Currencies>();
+                        options.MapEnum<TransactionType>();
+                        options.MapEnum<AccountType>();
+                    })
+                )
+                .AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()])
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
+                .AddScoped<IBankAccountsDbContext>(provider => provider.GetRequiredService<BankAccountsDbContext>())
+                .AddScoped<IAccountsRepositoryAsync, AccountsRepositoryAsync>()
+                .AddScoped<ITransactionsRepositoryAsync, TransactionsRepositoryAsync>()
+                .AddSingleton<ICurrencyService, CurrencyService.CurrencyService>()
+                .AddAutoMapper(options => options.AddMaps(Assembly.GetExecutingAssembly()))
+                .AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+                .AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+            
+            return services;
+        }
+
+        /// <summary>
+        /// Добавит и настроит обработчик ModelState ошибок, чтоб возвращал MbResult
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection SetupApiBehavior(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    KeyValuePair<string, ModelStateEntry?> error = context.ModelState
+                        .First(x => x.Value?.Errors.Count > 0);
+
+                    MbResult<object?> result = MbResult.Failure((int)HttpStatusCode.BadRequest, $"Validation error: {error.Value?.Errors[0].ErrorMessage} {error.Value?.Errors[0].Exception?.Message}");
+
+                    return new ObjectResult(result);
+                };
+            });
+            return services;
+        }
+
+        public static IServiceCollection SetupCors(this IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            }));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Добавит и настроит аутентификацию по токену 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection SetupAuthentication(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddAuthentication(config =>
+                {
+                    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.Authority = "http://localhost:7045";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)
+                        )
+                    };
+                });
+            
+            return services;
+        }
+
+        /// <summary>
+        /// Добавит и настроит все необходимые компоненты Hangfire
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection SetupHangfire(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHangfire(config => config.UsePostgreSqlStorage(c =>
+                    c.UseNpgsqlConnection(configuration.GetConnectionString("BankAccountsDbContext"))))
+                .AddHangfireServer()
+                .AddHostedService<JobsRegistrator>();
+            
+            return services;
+        }
+
+        /// <summary>
+        /// Добавит и настроит необходимые компоненты Swagger
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection SetupSwagger(this IServiceCollection services)
+        {
+            services
+                .AddEndpointsApiExplorer()
+                .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
+                .AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
--- BankAccounts.Api/Program.cs
+++ BankAccounts.Api/Program.cs
@@ -5,6 +5,7 @@ using BankAccounts.Api.Infrastructure;
 using BankAccounts.Api.Infrastructure.CurrencyService;
 using BankAccounts.Api.Infrastructure.Database.Context;
 using BankAccounts.Api.Infrastructure.Database.Migrator;
+using BankAccounts.Api.Infrastructure.Extensions;
 using BankAccounts.Api.Infrastructure.Hangfire;
 using BankAccounts.Api.Infrastructure.Hangfire.Registrator;
 using BankAccounts.Api.Infrastructure.Repository.Accounts;
@@ -29,82 +30,13 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 IServiceCollection services = builder.Services;
 
-// Common services
 services
-    .AddDbContext<BankAccountsDbContext>(optionsBuilder => 
-        optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BankAccountsDbContext)), options =>
-        {
-            options.MapEnum<Currencies>();
-            options.MapEnum<TransactionType>();
-            options.MapEnum<AccountType>();
-        })
-    )
-    .AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()])
-    .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
-    .AddScoped<IBankAccountsDbContext>(provider => provider.GetRequiredService<BankAccountsDbContext>())
-    .AddScoped<IAccountsRepositoryAsync, AccountsRepositoryAsync>()
-    .AddScoped<ITransactionsRepositoryAsync, TransactionsRepositoryAsync>()
-    .AddSingleton<ICurrencyService, CurrencyService>()
-    .AddAutoMapper(options => options.AddMaps(Assembly.GetExecutingAssembly()))
-    .AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
-    .AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
-    {
-        KeyValuePair<string, ModelStateEntry?> error = context.ModelState
-            .First(x => x.Value?.Errors.Count > 0);
-
-        MbResult<object?> result = MbResult.Failure((int)HttpStatusCode.BadRequest, $"Validation error: {error.Value?.Errors[0].ErrorMessage} {error.Value?.Errors[0].Exception?.Message}");
-
-        return new ObjectResult(result);
-    };
-});
-
-// Cors
-services.AddCors(options => options.AddPolicy("AllowAll", policy =>
-{
-    policy.AllowAnyHeader();
-    policy.AllowAnyMethod();
-    policy.AllowAnyOrigin();
-}));
-
-// Authentication
-services.AddAuthentication(config =>
-    {
-        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer("Bearer", options =>
-    {
-        options.RequireHttpsMetadata = false;
-        options.Authority = "http://localhost:7045";
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-            )
-        };
-    });
-
-// Hangfire
-services.AddHangfire(config => config.UseInMemoryStorage())
-    .AddHangfireServer()
-    .AddHostedService<JobsRegistrator>();
-
-// Swagger
-services
-    .AddEndpointsApiExplorer()
-    .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
-    .AddSwaggerGen();
+    .AddCommonServices(builder.Configuration)
+    .SetupApiBehavior()
+    .SetupCors()
+    .SetupAuthentication(builder.Configuration)
+    .SetupHangfire(builder.Configuration)
+    .SetupSwagger();
 
 WebApplication app = builder.Build();
 
