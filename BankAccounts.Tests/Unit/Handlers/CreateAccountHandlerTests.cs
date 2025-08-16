9ec041eb464f075ef78a3b32fe5410623070ed27
dem <dmitriy313.k@gmail.com>
2025-08-16 14:48:55 +0300

    test4: изменит CreateAccountHandlerTests для соответствия новой логике

--- BankAccounts.Tests/Unit/Handlers/CreateAccountHandlerTests.cs
+++ BankAccounts.Tests/Unit/Handlers/CreateAccountHandlerTests.cs
@@ -2,9 +2,14 @@
 using BankAccounts.Api.Features.Accounts;
 using BankAccounts.Api.Features.Accounts.Commands.CreateAccount;
 using BankAccounts.Api.Features.Accounts.Dtos;
+using BankAccounts.Api.Features.Shared;
 using BankAccounts.Api.Infrastructure.CurrencyService;
+using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Specific;
+using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
+using BankAccounts.Api.Infrastructure.Repository;
 using BankAccounts.Api.Infrastructure.Repository.Accounts;
 using Moq;
+using System.Transactions;
 
 namespace BankAccounts.Tests.Unit.Handlers;
 
@@ -15,12 +20,14 @@ public class CreateAccountHandlerTests
 {
     private readonly Mock<IAccountsRepositoryAsync> _mockRepository;
     private readonly Mock<IMapper> _mockMapper;
+    private readonly Mock<ISimpleTransactionScope> _mockTransactionScope;
     private readonly CreateAccountHandler _handler;
 
     public CreateAccountHandlerTests()
     {
         _mockRepository = new Mock<IAccountsRepositoryAsync>();
         _mockMapper = new Mock<IMapper>();
+        _mockTransactionScope = new Mock<ISimpleTransactionScope>();
         _handler = new CreateAccountHandler(_mockRepository.Object, _mockMapper.Object);
     }
 
@@ -50,12 +57,25 @@ public class CreateAccountHandlerTests
         };
         
         _mockRepository.Setup(r => r.AddAsync(account.OwnerId, account.AccountType, account.Currency, 
-                account.InterestRate, Guid.AllBitsSet, CancellationToken.None))
+                account.InterestRate, CausationIds.CreateAccount, CancellationToken.None))
             .ReturnsAsync(account); 
+        
+        _mockRepository.Setup(r => r.BeginSerializableTransactionAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_mockTransactionScope.Object);
+
+        _mockRepository.Setup(r => r.AddToOutboxAsync(
+                It.IsAny<AccountOpened>(), 
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask); 
         
         _mockMapper.Setup(m => m.Map<AccountDto>(account))
             .Returns((Account a) => new AccountDto(a.AccountId, a.AccountType, a.Currency, a.Balance, 
                 a.InterestRate, a.OpenDate, a.CloseDate));
+        
+        _mockTransactionScope.Setup(t => t.CommitAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        _mockTransactionScope.Setup(t => t.DisposeAsync())
+            .Returns(ValueTask.CompletedTask);
 
         // Act
         AccountDto result = await _handler.Handle(command, CancellationToken.None);
@@ -70,7 +90,7 @@ public class CreateAccountHandlerTests
        Assert.Equal(result.OpenDate, account.OpenDate);
        Assert.Equal(result.CloseDate, account.CloseDate);
        _mockRepository.Verify(r => r.AddAsync(account.OwnerId, account.AccountType, account.Currency,
-           account.InterestRate, Guid.AllBitsSet,  CancellationToken.None), Times.Once);
+           account.InterestRate, CausationIds.CreateAccount,  CancellationToken.None), Times.Once);
        _mockMapper.Verify(m => m.Map<AccountDto>(account), Times.Once);
     }
 }
