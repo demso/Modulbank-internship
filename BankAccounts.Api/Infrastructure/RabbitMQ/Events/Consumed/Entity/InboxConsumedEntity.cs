d794654c9793bd46e3d0229f7969644f453dc85d
dem <dmitriy313.k@gmail.com>
2025-08-16 04:19:42 +0300

    feat4: изменит using у InboxConsumedEntity

--- BankAccounts.Api/Infrastructure/RabbitMQ/Events/Consumed/Entity/InboxConsumedEntity.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Consumed/Entity/InboxConsumedEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Consumed.Entity
+﻿using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
+
+namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Consumed.Entity
 {
     public record InboxConsumedEntity(Guid Id, string Message, EventType EventType);
 }
