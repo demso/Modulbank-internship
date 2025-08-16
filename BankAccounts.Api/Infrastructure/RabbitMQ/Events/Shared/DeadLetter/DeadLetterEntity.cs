c4078ff997030c5f73b118eb884911b264cf27b0
dem <dmitriy313.k@gmail.com>
2025-08-17 00:59:45 +0300

    task4: добавит классы DeadLetterEntity и DeadLetterEntityConfiguration

---
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Shared/DeadLetter/DeadLetterEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared.DeadLetter
+{
+    public class DeadLetterEntity //inbox_dead_letters (message_id, received_at, handler, payload, error).
+    {
+        public Guid MessageId { get; set; }
+        public DateTime RecievedAt { get; set; }
+        public string Handler { get; set; }
+        public string Payload { get; set; }
+        public EventType? EventType { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Shared/DeadLetter/DeadLetterEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared.DeadLetter
+{
+    public class DeadLetterEntityConfiguration : IEntityTypeConfiguration<DeadLetterEntity>
+    {
+        public void Configure(EntityTypeBuilder<DeadLetterEntity> builder)
+        {
+            builder.ToTable("inbox_dead_letters");
+            builder.HasKey(l => l.MessageId);
+        }
+    }
+}
