b53626462234668cc3b45ae561355d7613726ac2
dem <dmitriy313.k@gmail.com>
2025-08-17 00:56:59 +0300

    task4: добавит новые поля в OutboxPublishedEntity

--- BankAccounts.Api/Infrastructure/RabbitMQ/Events/Published/Entity/OutboxPublishedEntity.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Published/Entity/OutboxPublishedEntity.cs
@@ -5,7 +5,10 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Entity
     public class OutboxPublishedEntity  
     {
         public Guid Id { get; init; }
-        public string Message { get; set; }
+        public required string Message { get; set; }
+        public  Guid EventId { get; set; }
+        public required Guid CorrelationId { get; set; }
+        public required Guid CausationId { get; set; }
         public EventType EventType { get; init; }
         public required DateTime Created { get; init; } 
         public uint TryCount { get; set; }
