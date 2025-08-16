9e298dd450de91299c27f644f74c473fec0ed61c
dem <dmitriy313.k@gmail.com>
2025-08-16 04:14:06 +0300

    task4: добавит новые типы в CausationIds

--- BankAccounts.Api/Features/Shared/CausationIds.cs
+++ BankAccounts.Api/Features/Shared/CausationIds.cs
@@ -6,11 +6,14 @@ namespace BankAccounts.Api.Features.Shared
     public static class CausationIds
     {
         public static Guid CreateAccount => CreateGuidFromString("CreateAccount");
+        public static Guid PerformTransaction => CreateGuidFromString("PerformTransaction");
+        public static Guid PerformTransfer => CreateGuidFromString("PerformTransfer");
+        public static Guid AccrueInterest => CreateGuidFromString("AccrueInterest");
 
         private static Guid CreateGuidFromString(string input)
         {
             byte[] hash = MD5.HashData(Encoding.UTF8.GetBytes(input));
-            Guid result = new Guid(hash);
+            Guid result = new(hash);
             return result;
         }
     }
