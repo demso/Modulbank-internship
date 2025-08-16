f2d9bd37c67f778e0d9e28339d69dec7c2dc2257
dem <dmitriy313.k@gmail.com>
2025-08-16 04:14:53 +0300

    task4: добавит поле TryCount в OutboxPublishedEntity

--- BankAccounts.Api/Infrastructure/RabbitMQ/Events/Published/Entity/OutboxPublishedEntity.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Published/Entity/OutboxPublishedEntity.cs
@@ -1,10 +1,13 @@
-﻿namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Entity
+﻿using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
+
+namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Entity
 {
     public class OutboxPublishedEntity  
     {
         public Guid Id { get; init; }
-        public string Message { get; init; }
+        public string Message { get; set; }
         public EventType EventType { get; init; }
         public required DateTime Created { get; init; } 
+        public uint TryCount { get; set; }
     }
 }
