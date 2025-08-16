288bceaa5fe8799b293941c594e450d8b0a315ea
dem <dmitriy313.k@gmail.com>
2025-08-16 04:17:00 +0300

    task4: добавит в PerformTransactionHandler добавление события в outbox

--- BankAccounts.Api/Features/Transactions/Commands/PerformTransaction/PerformTransactionHandler.cs
+++ BankAccounts.Api/Features/Transactions/Commands/PerformTransaction/PerformTransactionHandler.cs
@@ -3,6 +3,9 @@ using BankAccounts.Api.Common.Exceptions;
 using BankAccounts.Api.Features.Accounts;
 using BankAccounts.Api.Features.Shared;
 using BankAccounts.Api.Features.Transactions.Dtos;
+using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Specific;
+using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
+using BankAccounts.Api.Infrastructure.Repository;
 using BankAccounts.Api.Infrastructure.Repository.Accounts;
 using BankAccounts.Api.Infrastructure.Repository.Transactions;
 
@@ -14,36 +17,79 @@ namespace BankAccounts.Api.Features.Transactions.Commands.PerformTransaction;
 public class PerformTransactionHandler(IAccountsRepositoryAsync accountsRepository, ITransactionsRepositoryAsync transactionsRepository, 
     IMapper mapper) : RequestHandlerBase<PerformTransactionCommand, TransactionDto>
 {
+    private static readonly Guid CausationId = CausationIds.PerformTransaction;
+    
     /// <inheritdoc />
     public override async Task<TransactionDto> Handle(PerformTransactionCommand request, CancellationToken cancellationToken)
     {
         Account account = await GetValidAccount(accountsRepository, request.AccountId, request.OwnerId, cancellationToken);
 
-        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault Решарпер предлагает непонятный код
-        switch (request.TransactionType)
+        await using TransactionScope dbTransaction = await transactionsRepository.BeginSerializableTransactionAsync(cancellationToken);
+        
+        try
         {
-            case TransactionType.Debit:
-                account.Balance += request.Amount;
-                break;
-            case TransactionType.Credit:
-                if (account.AccountType is AccountType.Checking or AccountType.Deposit &&
-                    account.Balance - request.Amount < 0)
-                    throw new BadRequestException("Баланс после транзакции не может быть < 0, т.к. счет не является кредитным.");
-                account.Balance -= request.Amount;
-                break;
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault Решарпер предлагает непонятный код
+            switch (request.TransactionType) {
+                case TransactionType.Debit:
+                    account.Balance += request.Amount;
+                    break;
+                case TransactionType.Credit:
+                    if (account.AccountType is AccountType.Checking or AccountType.Deposit &&
+                        account.Balance - request.Amount < 0)
+                        throw new BadRequestException("Баланс после транзакции не может быть < 0, т.к. счет не является кредитным.");
+                    account.Balance -= request.Amount;
+                    break;
+            }
+
+            Transaction transaction = await transactionsRepository.AddAsync(
+                account.AccountId, 
+                0, 
+                request.Amount, 
+                account.Currency, 
+                request.TransactionType, 
+                request.Description, 
+                cancellationToken);
+
+            switch (request.TransactionType) {
+                case TransactionType.Debit:
+                    await transactionsRepository.AddToOutboxAsync(new MoneyCredited()
+                    {
+                        Amount = request.Amount,
+                        AccountId = transaction.AccountId,
+                        Currency = transaction.Currency,
+                        OperationId = transaction.TransactionId,
+                        Metadata = new Metadata
+                        {
+                            CausationId = CausationId,
+                        }
+                    }, cancellationToken);
+                    break;
+                case TransactionType.Credit:
+                    await transactionsRepository.AddToOutboxAsync(new MoneyDebited()
+                    {
+                        Amount = request.Amount,
+                        AccountId = transaction.AccountId,
+                        Currency = transaction.Currency,
+                        OperationId = transaction.TransactionId,
+                        Metadata = new Metadata
+                        {
+                            CausationId = CausationId,
+                        }
+                    }, cancellationToken);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(request), request.TransactionType, null);
+            }
+
+            await transactionsRepository.SaveChangesAsync(cancellationToken);
+            await dbTransaction.CommitAsync(cancellationToken);
+
+            return mapper.Map<TransactionDto>(transaction);
         }
-
-        Transaction transaction = await transactionsRepository.AddAsync(
-            account.AccountId, 
-            0, 
-            request.Amount, 
-            account.Currency, 
-            request.TransactionType, 
-            request.Description, 
-            cancellationToken);
-
-        await transactionsRepository.SaveChangesAsync(cancellationToken);
-
-        return mapper.Map<TransactionDto>(transaction);
+        catch (Exception ex)
+        {
+            const string message = "Account not opened due to an error. ";
+            throw new PerformTransactionException(message, ex);
+        }
     }
 }
