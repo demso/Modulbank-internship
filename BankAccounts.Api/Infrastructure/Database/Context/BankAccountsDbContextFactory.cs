ea83b871c11400889bc5f4470147eb95bddb334a
dem <dmitriy313.k@gmail.com>
2025-08-17 01:03:00 +0300

    task4: добавит фабрику BankAccountsDbContextFactory для создания миграций без ошибок

---
+++ BankAccounts.Api/Infrastructure/Database/Context/BankAccountsDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using BankAccounts.Api.Features.Accounts;
+using BankAccounts.Api.Features.Transactions;
+using BankAccounts.Api.Infrastructure.CurrencyService;
+using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BankAccounts.Api.Infrastructure.Database.Context;
+
+/// <summary>
+/// Для миграций
+/// </summary>
+public class BankAccountsDbContextFactory : IDesignTimeDbContextFactory<BankAccountsDbContext>
+{
+    public BankAccountsDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BankAccountsDbContext>();
+        
+        // Используйте вашу строку подключения
+        var connectionString = "";
+
+        optionsBuilder.UseNpgsql(connectionString, options =>
+        {
+            options.MapEnum<Currencies>();
+            options.MapEnum<TransactionType>();
+            options.MapEnum<AccountType>();
+            options.MapEnum<EventType>();
+        });
+        
+        return new BankAccountsDbContext(optionsBuilder.Options);
+    }
+}
