fda28fe47a9b2b7ad9927a1334761328cdeaaa28
dem <dmitriy313.k@gmail.com>
2025-08-17 22:52:37 +0300

    task4: добавит ограничения на поля в InboxEntityConfiguration

--- BankAccounts.Api/Infrastructure/RabbitMQ/Events/Consumed/Entity/InboxEntityConfiguration.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Consumed/Entity/InboxEntityConfiguration.cs
@@ -3,12 +3,19 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Consumed.Entity
 {
+    /// <inheritdoc />
     public class InboxEntityConfiguration :  IEntityTypeConfiguration<InboxConsumedEntity>
     {
+        /// <inheritdoc />
         public void Configure(EntityTypeBuilder<InboxConsumedEntity> builder)
         {
             builder.ToTable("inbox_consumed");
-            builder.HasKey(i => i.MessageId);
+            builder.HasKey(i => i.Id);
+            builder.HasIndex(i => i.MessageId)
+                .IsUnique();
+            builder.Property(i => i.Handler)
+                .HasMaxLength(256);
+            
         }
     }
 }
