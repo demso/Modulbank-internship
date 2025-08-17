933b13545b801f5f369ffa77a0f9a93ed34aaaf3
dem <dmitriy313.k@gmail.com>
2025-08-17 22:54:37 +0300

    task4: добавит MessageId в совйства сообщения при отправке в Sender

--- BankAccounts.Api/Infrastructure/Hangfire/Jobs/Sender.cs
+++ BankAccounts.Api/Infrastructure/Hangfire/Jobs/Sender.cs
@@ -1,5 +1,4 @@
 ﻿using BankAccounts.Api.Infrastructure.Database.Context;
-using BankAccounts.Api.Infrastructure.RabbitMQ;
 using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Published.Entity;
 using BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared;
 using Hangfire;
@@ -10,82 +9,94 @@ using System.Text;
 
 namespace BankAccounts.Api.Infrastructure.Hangfire.Jobs
 {
-    // Рекомендуется использовать Retry с экспоненциальной паузой и джиттером; при достижении MAX_RETRIES — статус dead‑letter, алерт в логах.
+    
+    /// <summary>
+    /// Отправщик событий
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="dbContext"></param>
+    /// <param name="configuration"></param>
     public class Sender(ILogger<Sender> logger, IBankAccountsDbContext dbContext, IConfiguration configuration)
     {
-        const ushort MAX_OUTSTANDING_CONFIRMS = 256;
-        int MESSAGE_COUNT = 1000;
-        ConnectionFactory factory = new()
+        private const ushort MaxOutstandingConfirms = 256;
+        private ConnectionFactory _factory = new()
         {
             HostName = configuration["RabbitMQ:Hostname"]!, 
             UserName = configuration["RabbitMQ:Username"]!, 
-            Password = configuration["RabbitMQ:Password"]!,
+            Password = configuration["RabbitMQ:Password"]!
         };
-        private IConnection? connection;
-        private IChannel? channel;
-        private const string ExchangeName = Receiver.ExchangeName;
-        BasicProperties props = new()
+        private IConnection? _connection;
+        private IChannel? _channel;
+        private readonly string _exchangeName = configuration["RabbitMQ:ExchangeName"]!;
+        private BasicProperties _props = new()
         {
             Persistent = true,
             Headers = new Dictionary<string, object?>()
         };
         
+        /// <summary>
+        /// Инициализация подключения и канала
+        /// </summary>
         public async Task Init()
         {
-            connection = await factory.CreateConnectionAsync();
+            _connection = await _factory.CreateConnectionAsync();
             
             CreateChannelOptions channelOptions = new (
                 publisherConfirmationsEnabled: true,
                 publisherConfirmationTrackingEnabled: true,
-                outstandingPublisherConfirmationsRateLimiter: new ThrottlingRateLimiter(MAX_OUTSTANDING_CONFIRMS)
+                outstandingPublisherConfirmationsRateLimiter: new ThrottlingRateLimiter(MaxOutstandingConfirms)
             );
             
-            channel = await connection.CreateChannelAsync(channelOptions);
+            _channel = await _connection.CreateChannelAsync(channelOptions);
             
-            await channel.BasicQosAsync(
+            await _channel.BasicQosAsync(
                 prefetchSize: 0,
                 prefetchCount: 1,
                 global: false
             );
         }
 
+        /// <summary>
+        /// Выполнение отправки сообщений из outbox
+        /// </summary>
         [AutomaticRetry(Attempts = 0)]
         public async Task Job()
         {
-            if (connection is null || connection.IsOpen == false || channel is null || channel.IsClosed == false)
+            if (_connection is null || _connection.IsOpen == false || _channel is null || _channel.IsClosed == false)
                await Init();
             
             Stopwatch sw = Stopwatch.StartNew();
 
             List<(OutboxPublishedEntity, ValueTask)> publishTasks = [];
-            //var successfulGuids = new List<Guid>();
 
             List<OutboxPublishedEntity> entities = await dbContext.OutboxPublished.ToListAsync();
             
             int publishedCount = await Send(publishTasks, entities);
             
              sw.Stop();
-             int count = (await dbContext.OutboxPublished.CountAsync());
-             logger.LogInformation($"Published {publishedCount} messages (failed and queued for retry: {count}) in batch in {sw.ElapsedMilliseconds:N0} ms");
+             int count = await dbContext.OutboxPublished.CountAsync();
+             logger.LogInformation("Published {PublishedCount} messages (failed and queued for retry: {Count}) " +
+                        "in batch in {SwElapsedMilliseconds:N0} ms", publishedCount, count, sw.ElapsedMilliseconds);
         }
 
         private async Task<int> Send(List<(OutboxPublishedEntity, ValueTask)> publishTasks, List<OutboxPublishedEntity> entities)
         {
-            int batchSize = Math.Max(1, MAX_OUTSTANDING_CONFIRMS / 2);
+            int batchSize = Math.Max(1, MaxOutstandingConfirms / 2);
             int succeededPublishes = 0;
             
             foreach (OutboxPublishedEntity entity in entities)
             {
                 string message = entity.Message;
-                var body = Encoding.UTF8.GetBytes(message);
+                byte[] body = Encoding.UTF8.GetBytes(message);
                 
-                props.Headers!["type"] = entity.EventType.ToString();
-                props.Headers["x-correlation-id"] = entity.CorrelationId.ToString();
-                props.Headers["x-causation-id"] = entity.CausationId.ToString();
-                props.Timestamp = new AmqpTimestamp(DateTime.UtcNow.ToBinary());
+                _props.Headers!["type"] = entity.EventType.ToString();
+                _props.Headers["x-correlation-id"] = entity.CorrelationId.ToString();
+                _props.Headers["x-causation-id"] = entity.CausationId.ToString();
+                _props.Timestamp = new AmqpTimestamp(DateTime.UtcNow.ToBinary());
+                _props.MessageId = entity.Id.ToString();
                 
-                ValueTask publishTask = channel!.BasicPublishAsync(exchange: ExchangeName, routingKey: Event.GetRoute(entity.EventType), 
-                    body: body, basicProperties: props, mandatory: false);
+                ValueTask publishTask = _channel!.BasicPublishAsync(exchange: _exchangeName, routingKey: Event.GetRoute(entity.EventType), 
+                    body: body, basicProperties: _props, mandatory: false);
                 publishTasks.Add((entity, publishTask));
                 
                 succeededPublishes += await MaybeAwaitPublishes(publishTasks, batchSize);
@@ -97,32 +108,35 @@ namespace BankAccounts.Api.Infrastructure.Hangfire.Jobs
             
             return succeededPublishes;
         }
-        
-        async Task<int> MaybeAwaitPublishes(List<(OutboxPublishedEntity, ValueTask)> publishTasks, int batchSize)
+
+        private async Task<int> MaybeAwaitPublishes(List<(OutboxPublishedEntity, ValueTask)> publishTasks, int batchSize)
         {
             int succedeedTasks = 0;
-            if (publishTasks.Count >= batchSize)
+            
+            if (publishTasks.Count < batchSize)
             {
-                foreach ((OutboxPublishedEntity entity, ValueTask pt)  in publishTasks)
-                {
-                    try
-                    {
-                        await pt;
-                        dbContext.OutboxPublished.Remove(entity);
-                        await dbContext.SaveChangesAsync(CancellationToken.None);
-                        succedeedTasks++;
-                        LogSuccess(entity);
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError("Error during event publishing: '{Exception}'", ex);
-                        entity.TryCount += 1;
-                        dbContext.OutboxPublished.Update(entity);
-                        await dbContext.SaveChangesAsync(CancellationToken.None);
-                    }
-                }
-                publishTasks.Clear();
+                return succedeedTasks;
             }
+
+            foreach ((OutboxPublishedEntity entity, ValueTask pt)  in publishTasks)
+            {
+                try
+                {
+                    await pt;
+                    dbContext.OutboxPublished.Remove(entity);
+                    await dbContext.SaveChangesAsync(CancellationToken.None);
+                    succedeedTasks++;
+                    LogSuccess(entity);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("Error during event publishing: '{Exception}'", ex);
+                    entity.TryCount += 1;
+                    dbContext.OutboxPublished.Update(entity);
+                    await dbContext.SaveChangesAsync(CancellationToken.None);
+                }
+            }
+            publishTasks.Clear();
             return succedeedTasks;
         }
 
