d15432523b9891fca55cdaa37014f0aeed39a54b
dem <dmitriy313.k@gmail.com>
2025-08-17 18:29:46 +0300

    task4: добавит логику получения и обработки запросов, логгирование в Receiver

--- BankAccounts.Api/Infrastructure/RabbitMQ/Receiver.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Receiver.cs
@@ -9,7 +9,7 @@ using System.Text.Json;
 
 namespace BankAccounts.Api.Infrastructure.RabbitMQ
 {
-    public class Receiver(ILogger<Receiver> logger, IBankAccountsDbContext dbContext) : IHostedService
+    public class Receiver(ILogger<Receiver> logger, IServiceScopeFactory scopeFactory, IConfiguration configuration) : IHostedService
     {
         private ConnectionFactory factory;
         private IConnection connection;
@@ -37,8 +37,6 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ
             await channel.ExchangeDeclareAsync(exchange: ExchangeName, type: ExchangeType.Topic);
             await SetupQueues();
             
-            
-            
             AsyncEventingBasicConsumer auditConsumer = new(channel);
             auditConsumer.ReceivedAsync += ProcessAuditMessage;
             
@@ -51,103 +49,186 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ
 
         private async Task ProcessAntifraudMessage(object model, BasicDeliverEventArgs ea)
         {
+            using var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<IBankAccountsDbContext>();
             byte[] body = ea.Body.ToArray();
             string message = Encoding.UTF8.GetString(body);
             await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);  
             logger.LogInformation($"Received {message}");
         }
 
+        private async Task DeadLetter(IBankAccountsDbContext dbContext, JsonDocument message, EventType? eventType, BasicDeliverEventArgs ea, string reason)
+        {
+            
+            LogDeadLetter(message, eventType, reason);
+            await AddToDeadLetters(dbContext, message.RootElement.GetRawText(), eventType, DateTime.UtcNow, "Receiver",
+                reason);
+            await channel.BasicNackAsync(deliveryTag: ea.DeliveryTag, multiple: false, requeue: false);
+        }
+
         private async Task ProcessAuditMessage(object model, BasicDeliverEventArgs ea)
         {
+            using var scope = scopeFactory.CreateScope();
+            IBankAccountsDbContext dbContext = scope.ServiceProvider.GetRequiredService<IBankAccountsDbContext>();
             byte[] body = ea.Body.ToArray();
-            string message = Encoding.UTF8.GetString(body);
-            bool hasType = Enum.TryParse(ea.BasicProperties.Headers["type"].ToString(), out EventType eventType);
-            if (ea.BasicProperties.Headers == null || ea.BasicProperties.Headers.Count == 0 || !hasType)
-            {
-                LogDeadLetter(message, hasType ? eventType : null);
-                await AddToDeadLetters(message, hasType ? eventType : null, DateTime.UtcNow, "Receiver", "something went wrong");
-                await channel.BasicNackAsync(deliveryTag: ea.DeliveryTag, multiple: false, requeue: false);
-                return;
-            }
+            string message = BytesToString(body);
+            JsonDocument document = JsonDocument.Parse(message);
             
-            InboxConsumedEntity? result = await AddToInbox(eventType, DateTime.UtcNow, "Receiver");
+            bool hasHeaders = false;
             
-            if (result != null)
+            EventType? nullableEventType = null;
+            
+            string headerCausationId = null!;
+            string headerCorrelationId = null!;
+
+            string version = null!;
+            string bodyCausationId = null!;
+            string bodyCorrelationId = null!;
+
+            string reason = null!;
+            
+            try
             {
-                await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
-                LogSuccess(message, eventType);
+                IDictionary<string, object?> headers = ea.BasicProperties.Headers!;
+                hasHeaders = headers.Count != 0;
+                
+                EventType eventType = Enum.Parse<EventType>(BytesToString(headers["type"]!));
+                nullableEventType = eventType;
+                
+                headerCausationId = BytesToString(headers["x-causation-id"]!);
+                headerCorrelationId = BytesToString(headers["x-correlation-id"]!);
+
+                version = document.RootElement.GetProperty("Metadata").GetProperty("Version").GetString()!;
+                bodyCausationId =
+                    document.RootElement.GetProperty("Metadata").GetProperty("CausationId").GetString()!;
+                bodyCorrelationId = document.RootElement.GetProperty("Metadata").GetProperty("CorrelationId")
+                    .GetString()!;
+
+                if (headerCausationId != bodyCausationId)
+                {
+                    reason = "Causation id mismatch";
+                    throw new Exception();
+                }
+
+                if (headerCorrelationId != bodyCorrelationId)
+                {
+                    reason = "Correlation id mismatch";
+                    throw new Exception();
+                }
+
+                if (int.Parse(version.Substring(1, 1)) < 1) // проверка на соответствие версии
+                {
+                    reason = "Message version not supported";
+                    throw new Exception();
+                }
+                
+                InboxConsumedEntity? result = await AddToInbox(dbContext, eventType, DateTime.UtcNow, "Receiver");
+                    
+                if (result != null)
+                {
+                    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+                    var propTimestamp = ea.BasicProperties.Timestamp.UnixTime;
+                    var timestamp = propTimestamp == 0 ? (DateTime?) null : DateTime.FromBinary(propTimestamp);
+                    LogSuccess(document, eventType, timestamp);
+                }
+                else
+                {
+                    await channel.BasicNackAsync(deliveryTag: ea.DeliveryTag, multiple: false, requeue: true);
+                }
+            
             }
-            else
+            catch (Exception)
             {
-                await channel.BasicNackAsync(deliveryTag: ea.DeliveryTag, multiple: false, requeue: true);
+                if (reason is null)
+                {
+                    if (!hasHeaders)
+                        reason = "No headers";
+                    else if (headerCausationId is null)
+                        reason = "No header causation";
+                    else if (headerCorrelationId is null)
+                        reason = "No header correlation";
+                    else if (version is null)
+                        reason = "No version";
+                    else if (bodyCausationId is null)
+                        reason = "No body causation";
+                    else if (bodyCorrelationId is null)
+                        reason = "No body correlation";
+                    else 
+                        reason = "Unknown reason";
+                }
+                
+                await DeadLetter(dbContext, document, nullableEventType, ea, reason);
             }
         }
+
+        private string BytesToString(object bytes)
+        {
+            return Encoding.UTF8.GetString((byte[]) bytes);
+        }
         
-        private void LogSuccess(string message, EventType type)
+        private void LogSuccess(JsonDocument document, EventType type, DateTime? timestamp)
         {
             string? id = null;
             string? correlationId = null;
-            DateTime createdAt = DateTime.MinValue;
-            TimeSpan? latency = createdAt == DateTime.MinValue ? null : DateTime.UtcNow - createdAt;
-
+            
             try
             {
-                using JsonDocument document = JsonDocument.Parse(message);
                 JsonElement root = document.RootElement;
                     
-                id = root.GetProperty("eventid").GetString();
-                correlationId = root.GetProperty("metadata").GetProperty("correlationid").GetString();
-                DateTime.TryParse(root.GetProperty("createdAt").GetString(), out createdAt);
+                id = root.GetProperty("EventId").GetString();
+                correlationId = root.GetProperty("Metadata").GetProperty("CorrelationId").GetString();
             }
             catch (Exception) {/* ignored */ }
+            
+            TimeSpan? latency = timestamp == null ? null : DateTime.UtcNow - timestamp;
                     
             logger.LogInformation("Successfully consumed event: id = {id}, type = {type}, " +
                                   "correlationId = {correlationId}, latency = {latency}", id, 
                 type.ToString(), correlationId, latency); // eventId, type, correlationId, retry, latency
         }
         
-        private void LogDeadLetter(string message, EventType? type)
+        private void LogDeadLetter(JsonDocument document, EventType? type, string reason)
         {
             string? id = null;
             string? correlationId = null;
-            DateTime createdAt = DateTime.MinValue;
-            TimeSpan? latency = createdAt == DateTime.MinValue ? null : DateTime.UtcNow - createdAt;
 
             try
             {
-                using JsonDocument document = JsonDocument.Parse(message);
                 JsonElement root = document.RootElement;
                     
-                id = root.GetProperty("eventid").GetString();
-                correlationId = root.GetProperty("metadata").GetProperty("correlationid").GetString();
-                DateTime.TryParse(root.GetProperty("createdAt").GetString(), out createdAt);
+                id = root.GetProperty("EventId").GetString();
+                correlationId = root.GetProperty("Metadata").GetProperty("CorrelationId").GetString();
             }
             catch (Exception) {/* ignored */ }
                     
             logger.LogInformation("Consumed \"dead letter\" event: id = {id}, type = {type}, " +
-                                  "correlationId = {correlationId}, latency = {latency}", id, 
-                type.ToString(), correlationId, latency); // eventId, type, correlationId, retry, latency
+                                  "correlationId = {correlationId}, reason = {reason}", id, 
+                type.ToString(), correlationId, reason); // eventId, type, correlationId, retry, latency
         }
 
-        private async Task<InboxConsumedEntity?> AddToInbox(EventType eventType, DateTime processedAt, string handler)
+        private async Task<InboxConsumedEntity?> AddToInbox(IBankAccountsDbContext dbContext, EventType eventType, DateTime processedAt, string handler)
         {
             InboxConsumedEntity? entity = null;
             try
             {
                 InboxConsumedEntity inboxConsumed = new()
                 {
-                    EventType = eventType, ProcessedAt = processedAt, Handler = handler
+                    EventType = eventType, 
+                    ProcessedAt = processedAt, 
+                    Handler = handler
                 };
             
                 entity = (await dbContext.InboxConsumed.AddAsync(inboxConsumed)).Entity;
                 await dbContext.SaveChangesAsync(CancellationToken.None);
 
-            } catch (Exception e) { logger.LogError("{Message}","Failed to add message to inbox.\n" + e.Message + "\n" + e.StackTrace); }
+            } catch (Exception e) { logger.LogError("{Message}",
+                "Failed to add message to inbox.\n" + e.Message + "\n" + e.StackTrace); }
             
             return entity;
         }
         
-        private async Task<DeadLetterEntity?> AddToDeadLetters(string message,EventType? eventType, DateTime recievedAt, string handler, string error)
+        private async Task<DeadLetterEntity?> AddToDeadLetters(IBankAccountsDbContext dbContext, string message, EventType? eventType, 
+            DateTime recievedAt, string handler, string error)
         {
             DeadLetterEntity? entity = null;
             
@@ -164,7 +245,8 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ
                 entity = (await dbContext.DeadLetters.AddAsync(deadLetter)).Entity;
                 await dbContext.SaveChangesAsync(CancellationToken.None);
                 
-            } catch (Exception e) { logger.LogError("{Message}","Failed to add message to dead letters.\n" + e.Message + "\n" + e.StackTrace); }
+            } catch (Exception e) { logger.LogError("{Message}",
+                "Failed to add message to dead letters.\n" + e.Message + "\n" + e.StackTrace); }
             
             return entity;
         }
@@ -181,7 +263,7 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ
                 autoDelete: false,
                 arguments: null);
             
-            //await channel.QueueBindAsync(AntifraudQueueName, ExchangeName, "client.#");
+            await channel.QueueBindAsync(AntifraudQueueName, ExchangeName, "client.#");
 
             await channel.QueueDeclareAsync(
                 queue: AuditQueueName, 
@@ -191,7 +273,7 @@ namespace BankAccounts.Api.Infrastructure.RabbitMQ
                 arguments: null
             );
             
-            //await channel.QueueBindAsync(AuditQueueName, ExchangeName, "#");
+            await channel.QueueBindAsync(AuditQueueName, ExchangeName, "#");
             
             foreach (EventType type in Enum.GetValues<EventType>())
             {
