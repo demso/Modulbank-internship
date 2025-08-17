599a6b502e9866d32fd965937951e29c633b6334
dem <dmitriy313.k@gmail.com>
2025-08-17 22:52:17 +0300

    task4: добавит ограничение на поля в DeadLetterEntityConfiguration

--- BankAccounts.Api/Infrastructure/RabbitMQ/Events/Shared/DeadLetter/DeadLetterEntityConfiguration.cs
+++ BankAccounts.Api/Infrastructure/RabbitMQ/Events/Shared/DeadLetter/DeadLetterEntityConfiguration.cs
@@ -3,12 +3,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BankAccounts.Api.Infrastructure.RabbitMQ.Events.Shared.DeadLetter
 {
+    /// <summary>
+    /// Конфигурация сущности таблицы с мертвыми сообщениями inbox_dead_letter <see cref="DeadLetterEntity"/>
+    /// </summary>
     public class DeadLetterEntityConfiguration : IEntityTypeConfiguration<DeadLetterEntity>
     {
+        /// <inheritdoc />
         public void Configure(EntityTypeBuilder<DeadLetterEntity> builder)
         {
             builder.ToTable("inbox_dead_letters");
-            builder.HasKey(l => l.MessageId);
+            builder.HasKey(l => l.Id);
+            builder.HasIndex(l => l.MessageId)
+                .IsUnique();
+            builder.Property(l => l.Handler)
+                .HasMaxLength(256);
+            builder.Property(l => l.Error)
+                .HasMaxLength(256);
+            builder.Property(l => l.Payload)
+                .HasMaxLength(int.MaxValue);
         }
     }
 }
